0fcb4cb69a42aac8dac6275b7029cb463798817b
Sarah Andrew <sarah.andrew@stu.ocsb.ca>
2023-02-10 01:08:01 +0000

    Made a program that does the following operations.

---
+++ MathOperators.swift
@@ -0,0 +1,36 @@
+//
+
+//  This program executes the following operations, such as addition, etc.
+
+//
+//  Created by Sarah Andrew
+
+//  Created on 2023-02-09
+
+//  Version 1.0
+
+//  Copyright (c) Year Sarah. All rights reserved.
+import Foundation
+
+// Addition .
+print("25 + 5 = ", 25 + 5)
+// Subtraction
+print("7 - 5 = ", 7 - 5)
+// Multiplication.
+print("8 * 4 = ", 8 * 4)
+// Division with ints.
+print("36 / 2 = ", 36 / 2)
+// Division with floats.
+print("5 / 7 = ", 5.0 / 7.0)
+// Division with double.
+print("145 / 12 = ", 145.0 / 12.0)
+// Exponent raised to 2.
+print("10^2 =  ", pow(10.0, 2.0))
+// Exponent raised to 3.
+print("5^3 = ", pow(5.0, 3.0))
+// Square root.
+print("√144 = ", sqrt(144))
+
+
+ 
+
